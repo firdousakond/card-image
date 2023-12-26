fc49474c9035e6fd11fbddb117cd7c5bcc74043e
firdousakond <shahanifirdous@gmail.com>
2023-12-26 18:26:17 +0800

    Gradle updated

--- card-image/build.gradle.kts
+++ card-image/build.gradle.kts
@@ -24,11 +24,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
